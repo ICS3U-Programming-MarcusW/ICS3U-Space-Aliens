775b24455eaba72182ef1c7045f0e7a0cccde0e3
Marcus Wehbi <marcus.wehbi@stu.ocsb.ca>
2023-01-10 02:56:46 +0000

    Created game on PyBadge

---
+++ code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Marcus Wehbi
+# Created on: January 2023
+# This program is the "Pyramid Escape" program on the PyBadge
+
+import ugame
+import stage
+
+
+def game_scene():
+    # This function is in charge of the main game scene
+
+    print("\n\n\n")  # Three blank lines
+    print("Hello, World!")  # 'Hello' Statement
+
+    # Loop to repeat the game forever
+    while True:
+        pass  # Placeholder for future code
+
+
+if __name__ == "__main__":
+    game_scene()
