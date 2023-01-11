c32b621f930cfac7b33c201f512659fb23034e1f
Marcus Wehbi <marcus.wehbi@stu.ocsb.ca>
2023-01-11 03:57:30 +0000

    Added sprite

--- code.py
+++ code.py
@@ -1,37 +1,45 @@
 #!/usr/bin/env python3
 
-# This program is the "Pyramid Escape" game for the PyBadge.
-# It is written in Python and uses the ugame and stage libraries
-# to manage the graphics and input for the game.
+# This program is the "Space Aliens" game for the PyBadge.
+# It uses the ugame and stage libraries to manage the graphics
+# and input for the game.
 
 import ugame
 import stage
 
 
 def game_scene():
-    # This function is in charge of setting up and running the main game scene.
+    # This function sets up and runs the main game scene.
 
-    # Load the background image and create a "Grid" object using the image
-    # and a size of 10x8 tiles.
-    image_bank_background = stage.Bank.from_bmp16("pacBackgroundImg.bmp")
+    # Load the background and sprite image banks
+    image_bank_background = stage.Bank.from_bmp16
+    ("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+
+    # Create the background grid using the image and set the size to 10x8 tiles
     background = stage.Grid(image_bank_background, 10, 8)
 
-    # Create a "Stage" object to manage the game graphics and input,
-    # and set the frame rate to 60fps.
+    # Create the ship sprite using image at index 5, with initial position
+    # (72,57)
+    ship = stage.Sprite(image_bank_sprites, 5, 72, 57)
+
+    # Create a "Stage" object to manage the game graphics and input
+    # Set the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
 
-    # Set the "layers" property of the "Stage" object to a list containing
-    # the "Grid" object, which serves as the background for the game.
-    game.layers = [background]
+    # Add the background and ship to the layers list
+    game.layers = [ship] + [background]
 
-    # Render the background image on the screen.
+    # Draw the background on the screen
     game.render_block()
 
-    # Enter an infinite loop to update the game and handle user input.
+    # Game Loop
     while True:
-        pass  # Placeholder for future code
+        # Redraw the sprites on the screen
+        game.render_sprites([ship])
+        # Pause the loop to achieve 60fps frame rate
+        game.tick()
 
 
-# Run the "game_scene()" function when the program is executed.
 if __name__ == "__main__":
     game_scene()
